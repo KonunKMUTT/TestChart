4e332d139a7042d6bcd92eb11983b45de139d6b3
KonunKMUTT <156557056+KonunKMUTT@users.noreply.github.com>
2024-02-12 16:13:11 +0700

    Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -12,18 +12,4 @@ ext_data = conn.read(wroksheet="Sheet1", usecols=list(range(10)), ttl=5)
 
 # ลบข้อมูล NaN ทั้งหมด
 ext_data = ext_data.dropna(how="all")
-
-# แยกข้อมูล Class, Female และ Male
-classes = ext_data["Class"]
-females = ext_data["Female"]
-males = ext_data["Male"]
-
-# วาด Bar chart
-plt.bar(classes, females, label="Female")
-plt.bar(classes, males, bottom=females, label="Male")
-plt.xlabel("Class")
-plt.ylabel("จำนวน")
-plt.legend()
-
-# แสดง Bar chart บน Streamlit
-st.pyplot(plt)
+st.dataframe(ext_data)
