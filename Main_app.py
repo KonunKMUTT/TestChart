e246ffba8343feda56303eacc9759f89146f85d0
KonunKMUTT <156557056+KonunKMUTT@users.noreply.github.com>
2024-02-12 16:34:22 +0700

    Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -14,4 +14,4 @@ ext_data = conn.read(wroksheet="Sheet1", usecols=list(range(10)), ttl=5)
 ext_data = ext_data.dropna(how="all")
 st.dataframe(ext_data)
 
-st.line_chart(ext_data["class"])
+st.line_chart(ext_data["female"])
